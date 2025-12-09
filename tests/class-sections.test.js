37df9b70dff6661c617d2980ff34676412093ea0
Mitch Hunter <144734462+utahphotohunter@users.noreply.github.com>
2025-12-08 23:07:03 -0700

    finished unit tests for class sections

--- tests/class-sections.test.js
+++ tests/class-sections.test.js
@@ -2,10 +2,11 @@ require("dotenv").config();
 const request = require("supertest");
 const mongodb = require("../db/connection");
 const { app } = require("../server");
+const { json } = require("express");
 
 jest.setTimeout(30000); // Set timeout to 30 seconds for DB operations
 
-describe("Class Section API Endpoints", () => {
+describe("Classes API Endpoints", () => {
   // Ensure DB connection before running tests
   beforeAll(async () => {
     await new Promise((resolve, reject) => {
@@ -23,141 +24,139 @@ describe("Class Section API Endpoints", () => {
     }
   });
 
-  // GET all sections
-  test("should return all sections for GET /class-sections", async () => {
-    const res = await request(app).get("/class-sections").expect(200);
+  // return all class sections
+  test("should return all class sections for GET /class-sections", async () => {
+    const res = await request(app).get("/classes").expect(200);
     expect(res.body).not.toBe(null);
   });
 
-  // POST new section
-  // test("should create new class section", async () => {
-  //   const section = {
-  //     classId: "6740f1a5c9e6f9a1b2328222",
-  //     sectionNumber: "213",
-  //     semester: "Winter 2029",
-  //     teacherId: "6740f1a5c9e6f9a1b9875221",
-  //     classroomId: "2690f1a5c9e6f9a1b1112311CS",
-  //     meetingTimes: [
-  //       {
-  //         day: "Mon",
-  //         start: "13:15",
-  //         end: "14:45",
-  //       },
-  //       {
-  //         day: "Wed",
-  //         start: "13:15",
-  //         end: "14:45",
-  //       },
-  //       {
-  //         day: "Fri",
-  //         start: "13:15",
-  //         end: "14:45",
-  //       },
-  //     ],
-  //     capacity: 35,
-  //   };
+  // create new class section
+  test("should create new class-section", async () => {
+    const newClassSection = {
+      sectionId: "MATH367-001",
+      classId: "MATH367",
+      semesterId: "winter2029",
+      teacherId: "T13356",
+      classroomId: "MATHB333",
+      meetingTimes: [
+        {
+          day: "Mon",
+          start: "07:30",
+          end: "09:30",
+        },
+        {
+          day: "Wed",
+          start: "07:30",
+          end: "09:30",
+        },
+        {
+          day: "Fri",
+          start: "07:30",
+          end: "09:30",
+        },
+      ],
+      capacity: 85,
+    };
 
-  //   const res = await request(app)
-  //     .post("/class-sections")
-  //     .send(section)
-  //     .expect(201);
-  // });
+    const res = await request(app)
+      .post("/class-sections/")
+      .send(newClassSection)
+      .expect(201);
+  });
 
-  // GET by id
-  test("should return created section", async () => {
-    const res = await request(app).get("/class-sections/001").expect(200);
+  // get class section by sectionId
+  test("should return created class section", async () => {
+    const res = await request(app)
+      .get("/class-sections/MATH367-001")
+      .expect(200);
     expect(res.body).not.toBe(null);
-    expect(res.body.classId).toBe("6740f1a5c9e6f9a1b2222222");
-    expect(res.body.sectionNumber).toBe("001");
-    expect(res.body.semester).toBe("Fall 2025");
-    expect(res.body.teacherId).toBe("6740f1a5c9e6f9a1b7654321");
-    expect(res.body.classroomId).toBe("6740f1a5c9e6f9a1b1111111");
-    expect(res.body.meetingTimes).toMatchObject([
-      {
-        day: "Mon",
-        start: "10:00",
-        end: "11:15",
-      },
-      {
-        day: "Wed",
-        start: "10:00",
-        end: "11:15",
-      },
-    ]);
-    expect(res.body.capacity).toBe(40);
-    // expect(res.body).toBeInstanceOf(Object);
+    expect(res.body.sectionId).toBe("MATH367-001");
+    expect(res.body.classId).toBe("MATH367");
+    expect(res.body.semesterId).toBe("winter2029");
+    expect(res.body.teacherId).toBe("T13356");
+    expect(res.body.classroomId).toBe("MATHB333");
+    expect(res.body.meetingTimes).toBeInstanceOf(Object);
+    expect(res.body.meetingTimes[0].day).toBe("Mon");
+    expect(res.body.meetingTimes[0].start).toBe("07:30");
+    expect(res.body.meetingTimes[0].end).toBe("09:30");
+    expect(res.body.meetingTimes[1].day).toBe("Wed");
+    expect(res.body.meetingTimes[1].start).toBe("07:30");
+    expect(res.body.meetingTimes[1].end).toBe("09:30");
+    expect(res.body.meetingTimes[2].day).toBe("Fri");
+    expect(res.body.meetingTimes[2].start).toBe("07:30");
+    expect(res.body.meetingTimes[2].end).toBe("09:30");
+    expect(res.body).toBeInstanceOf(Object);
+  });
+
+  // update class section by sectionId
+  test("should edit created class section", async () => {
+    const newClassSection = {
+      sectionId: "MATH367-001",
+      classId: "MATH367",
+      semesterId: "winter2029",
+      teacherId: "T12889",
+      classroomId: "MATHB250",
+      meetingTimes: [
+        {
+          day: "Mon",
+          start: "07:30",
+          end: "09:30",
+        },
+        {
+          day: "Wed",
+          start: "07:30",
+          end: "09:30",
+        },
+        {
+          day: "Fri",
+          start: "07:30",
+          end: "09:30",
+        },
+      ],
+      capacity: 85,
+    };
+
+    const res = await request(app)
+      .put("/class-sections/MATH367-001")
+      .send(newClassSection)
+      .expect(204);
+  });
+
+  // get updated class section by sectionId
+  test("should return updated class section", async () => {
+    const res = await request(app)
+      .get("/class-sections/MATH367-001")
+      .expect(200);
+    expect(res.body).not.toBe(null);
+    expect(res.body.sectionId).toBe("MATH367-001");
+    expect(res.body.classId).toBe("MATH367");
+    expect(res.body.semesterId).toBe("winter2029");
+    expect(res.body.teacherId).toBe("T12889");
+    expect(res.body.classroomId).toBe("MATHB250");
+    expect(res.body.meetingTimes).toBeInstanceOf(Object);
+    expect(res.body.meetingTimes[0].day).toBe("Mon");
+    expect(res.body.meetingTimes[0].start).toBe("07:30");
+    expect(res.body.meetingTimes[0].end).toBe("09:30");
+    expect(res.body.meetingTimes[1].day).toBe("Wed");
+    expect(res.body.meetingTimes[1].start).toBe("07:30");
+    expect(res.body.meetingTimes[1].end).toBe("09:30");
+    expect(res.body.meetingTimes[2].day).toBe("Fri");
+    expect(res.body.meetingTimes[2].start).toBe("07:30");
+    expect(res.body.meetingTimes[2].end).toBe("09:30");
+    expect(res.body).toBeInstanceOf(Object);
+  });
+
+  // delete class section by sectionId
+  test("should delete class section", async () => {
+    const res = await request(app).delete("/class-sections/MATH367-001").expect(204);
+  });
+
+  // test 404 error message for no sectionId
+  test("should return created 404 error message", async () => {
+    const res = await request(app)
+      .get("/class-sections/MATH367-001")
+      .expect(404);
+    expect(res.body).not.toBe(null);
+    expect(res.body).toBe("No data found with that section id.");
   });
 });
-
-//   test("should edit created section", async () => {
-//     const section = {
-//       classId: "6740f1a5c9e6f9a1b2328222",
-//       sectionNumber: "213",
-//       semester: "Fall 2029",
-//       teacherId: "9840f1a5c9e6f9a1b9875221",
-//       classroomId: "9890f1a5c9e6f9a1b1112311CS",
-//       meetingTimes: [
-//         {
-//           day: "Mon",
-//           start: "13:15",
-//           end: "14:45",
-//         },
-//         {
-//           day: "Wed",
-//           start: "13:15",
-//           end: "14:45",
-//         },
-//         {
-//           day: "Fri",
-//           start: "13:15",
-//           end: "14:45",
-//         },
-//       ],
-//       capacity: 30,
-//     };
-
-//     const res = await request(app)
-//       .put("/class-sections/213")
-//       .send(section)
-//       .expect(204);
-//   });
-
-//   test("should return updated section", async () => {
-//     const res = await request(app).get("/students/S543210").expect(200);
-//     expect(res.body).not.toBe(null);
-//     expect(res.body.classId).toBe("6740f1a5c9e6f9a1b2328222");
-//     expect(res.body.sectionNumber).toBe("213");
-//     expect(res.body.semester).toBe("Fall 2029");
-//     expect(res.body.teacherId).toBe("9840f1a5c9e6f9a1b9875221");
-//     expect(res.body.classroomId).toBe("9890f1a5c9e6f9a1b1112311CS");
-//     expect(res.body.meetingTimes).toBe([
-//       {
-//         day: "Mon",
-//         start: "13:15",
-//         end: "14:45",
-//       },
-//       {
-//         day: "Wed",
-//         start: "13:15",
-//         end: "14:45",
-//       },
-//       {
-//         day: "Fri",
-//         start: "13:15",
-//         end: "14:45",
-//       },
-//     ]);
-//     expect(res.body.capacity).toBe(30);
-//     expect(res.body).toBeInstanceOf(Object);
-//   });
-
-//   test("should delete section", async () => {
-//     const res = await request(app).delete("/class-sections/213").expect(204);
-//   });
-// });
-
-// test("should not deleted section", async () => {
-//   const res = await request(app).get("/class-sections/213").expect(200);
-//   expect(res.body).toBe("section number does not exist");
-//   expect(res.body).toBeInstanceOf(String);
-// });
